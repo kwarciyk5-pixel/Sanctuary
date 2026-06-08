b3ca70c33d48b4af2b4f2ce55ffd52adf0e613b7
kwarciyk5-pixel <kwarci.yk5@gmail.com>
2026-06-08 12:13:52 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'sanctuary-v1';
+const URLS_TO_CACHE = [
+  './sanctuary.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
